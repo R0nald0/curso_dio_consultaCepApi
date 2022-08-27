300d0fd3f061d49d8ad30b5a09f524e0c8335534
R0nald0 <ronaldo-172010@hotmail.com>
2022-08-27 01:17:48 -0300

    fixado erro ao editar usuario

--- src/main/java/com/example/consultacep/controller/UsuarioController.java
+++ src/main/java/com/example/consultacep/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import com.example.consultacep.model.Usuario;
 import com.example.consultacep.service.UsuarioService;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RequestMapping("/user")
 public class UsuarioController {
     
